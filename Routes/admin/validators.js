f9da8a132692452ce5b683f2f719866962b36d41
Sanjeet Uday Jha <sanjeetj@cybage.com>
2023-04-26 18:59:34 +0530

    Working on the middleware

--- Routes/admin/validators.js
+++ Routes/admin/validators.js
@@ -4,11 +4,13 @@ const usersRepo = require('../../repositories/users');
 module.exports = {
   requireTitle: check('title')
     .trim()
-    .isLength({ min: 5, max: 40 }),
+    .isLength({ min: 5, max: 40 })
+    .withMessage('Must be between 5 and 40 character'),
   requirePrice: check('price')
     .trim()
     .toFloat()
-    .isFloat({ min: 1 }),
+    .isFloat({ min: 1 })
+    .withMessage('Must be greater than 1'),
   requireEmail: check('email')
     .trim()
     .normalizeEmail()
